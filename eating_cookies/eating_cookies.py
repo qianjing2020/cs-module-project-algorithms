94b7898ea8c224e4b72f7bee997ee181a52f1a68
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-22 23:14:33 -0400

    finished module 3

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,31 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    # input n cookies in the cookie jar
+    # output numbero ways cookie monster can finish the jar
+    # cookie monster can eat 1, or 2, or 3 cookies at one time
+    # step 1: eat 1 or 2 or 3 cookies, so there are three branches (possible ways) for step 1
+    # step 2: eat the leftover n-1, n-2, n-3 cookies, sum all three branches to get the total number of ways to eat n cookies
+    # this can be recursively solved
+    # base case: 0 cookie, 1 ways this is trick, because the end branch leading to 0 cookies shoiuld stay as one way, not 0
+    if n == 0:
+        return 1 
+    # base case: 1 cookie, 1 way
+    if n == 1:
+        return 1
+    # base case: 2 cookies, 2 ways (1+1, or 2)
+    if n == 2:
+        return 2
+    # base case: 3 cookies, 4 ways (1+1+1, 1+2, 2+1, 2+2)
+    if n == 3:
+        return 4
+    
+    # recursive cases:    
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)  
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 4
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
